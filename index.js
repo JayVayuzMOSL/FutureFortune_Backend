aa993349ef36bd03303e71ececdf4efa7dd5cd7c
jaykadam <jay.kadam@vayuz.in>
2025-03-03 20:57:32 +0530

    changes added

--- index.js
+++ index.js
@@ -1,52 +1,60 @@
-const express = require("express");
-const bodyparser = require("body-parser");
-var admin = require("firebase-admin");
+import {initializeApp, applicationDefault } from 'firebase-admin/app';
+import { getMessaging } from "firebase-admin/messaging";
+import express, { json } from "express";
+import cors from "cors";
 
-admin.initializeApp({
-  credential: admin.credential.applicationDefault(),
+initializeApp({
+  credential: applicationDefault(),
 });
 
 const app = express();
-app.use(bodyparser.json());
+app.use(express.json());
 
-const port = 3000;
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
-const notification_options = {
-  priority: "high",
-  timeToLive: 60 * 60 * 24,
-};
+app.use(
+  cors({
+    methods: ["GET", "POST", "DELETE", "UPDATE", "PUT", "PATCH"],
+  })
+);
 
-app.post("/firebase/notification", (req, res) => {
-  const registrationToken = req.body.registrationToken;
+app.use(function(req, res, next) {
+  res.setHeader("Content-Type", "application/json");
+  next();
+});
 
+
+app.post("/send", function (req, res) {
+  const receivedToken = req.body.fcmToken;
+  
   const message = {
     notification: {
-      title: req.body.title,
-      body: req.body.body,
-      image: req.body.imageUrl,
-    }
+      title: "Notif",
+      body: 'This is a Test Notification'
+    },
+    token: "YOUR FCM TOKEN HERE",
   };
-
-  const options = notification_options;
-
-  admin
-    .messaging()
-    .sendToDevice(registrationToken, message, options)
+  
+  getMessaging()
+    .send(message)
     .then((response) => {
-      if (response.results[0].messageId == null) {
-        console.log(response.results[0].error);
-        res.status(400).send(response.results[0].error);
-      } else {
-        console.log(message);
-        res.status(200).send('Notification sent successfully with messageId: ' + response.results[0].messageId);
-      }
+      res.status(200).json({
+        message: "Successfully sent message",
+        token: receivedToken,
+      });
+      console.log("Successfully sent message:", response);
     })
     .catch((error) => {
-      console.log(error);
-      res.status(400).send(error);
+      res.status(400);
+      res.send(error);
+      console.log("Error sending message:", error);
     });
 });
 
-app.listen(port, () => {
-  console.log("listening to PORT = " + port);
+app.listen(3000, function () {
+  console.log("Server started on port 3000");
 });
